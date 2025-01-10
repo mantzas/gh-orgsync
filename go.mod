1a99e142306a2ebe7f556dbe9afaef942b165d35
Sotirios Mantziaris <smantziaris@gmail.com>
2025-01-10 09:38:53 +0200

    Upgrades and code owners

--- go.mod
+++ go.mod
@@ -2,16 +2,21 @@ module github.com/mantzas/gh-orgsync
 
 go 1.23.4
 
-require github.com/cli/go-gh v0.1.0
+require github.com/cli/go-gh v1.2.1
 
 require (
-	github.com/cli/safeexec v1.0.0 // indirect
-	github.com/cli/shurcooL-graphql v0.0.1 // indirect
-	github.com/henvic/httpretty v0.0.6 // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/cli/safeexec v1.0.1 // indirect
+	github.com/cli/shurcooL-graphql v0.0.4 // indirect
+	github.com/henvic/httpretty v0.1.4 // indirect
 	github.com/kr/text v0.2.0 // indirect
-	github.com/thlib/go-timezone-local v0.0.0-20210907160436-ef149e42d28e // indirect
-	golang.org/x/net v0.33.0 // indirect
-	golang.org/x/sys v0.28.0 // indirect
-	golang.org/x/term v0.27.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/thlib/go-timezone-local v0.0.6 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/term v0.28.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- vendor/github.com/thlib/go-timezone-local/tzlocal/tz_windows.go
+++ vendor/github.com/thlib/go-timezone-local/tzlocal/tz_windows.go
@@ -4,56 +4,114 @@ package tzlocal
 
 import (
 	"fmt"
-	"os/exec"
 	"strings"
+	"time"
 
 	"golang.org/x/sys/windows/registry"
 )
 
 const tzKey = `SYSTEM\CurrentControlSet\Control\TimeZoneInformation`
 const tzKeyVal = "TimeZoneKeyName"
+const tzDynamicDayLightDisabledKeyVal = "DynamicDaylightTimeDisabled"
 
 // LocalTZ obtains the name of the time zone Windows is configured to use. Returns the corresponding IANA standard name
 func LocalTZ() (string, error) {
-	var winTZname string
-	var errTzutil, errReg error
+	var dstOff bool
+	const dstOffSuffix = "_dstoff"
 
 	// try tzutil command first - if that is not available, try to read from registry
-	winTZname, errTzutil = localTZfromTzutil()
-	if errTzutil != nil {
-		winTZname, errReg = localTZfromReg()
+	winTZname, errTzUtil := localTZfromTzutil()
+	if strings.HasSuffix(winTZname, dstOffSuffix) {
+		winTZname = strings.TrimSuffix(winTZname, dstOffSuffix)
+		dstOff = true
+	}
+
+	// Try to read from registry
+	if errTzUtil != nil {
+		var errReg error
+		winTZname, dstOff, errReg = localTZfromReg()
 		if errReg != nil { // both methods failed, return both errors
-			return "", fmt.Errorf("failed to read time zone name with errors\n(1) %s\n(2) %s", errTzutil, errReg)
+			return "", fmt.Errorf("failed to read timezone from tzutil %s and registry %s", errTzUtil.Error(), errReg.Error())
 		}
 	}
 
-	if name, ok := WinTZtoIANA[winTZname]; ok {
-		return name, nil
+	// Get the IANA time zone from the set time zone.
+	timezone, ok := WinTZtoIANA[winTZname]
+	if !ok {
+		return "", fmt.Errorf("could not find IANA tz name for set time zone %q", winTZname)
 	}
-	return "", fmt.Errorf("could not find IANA tz name for set time zone \"%s\"", winTZname)
-}
 
-// localTZfromTzutil executes command `tzutil /g` to get the name of the time zone Windows is configured to use.
-func localTZfromTzutil() (string, error) {
-	cmd := exec.Command("tzutil", "/g")
-	data, err := cmd.Output()
-	if err != nil {
-		return "", err
+	// Check if registry key `DynamicDaylightTimeDisabled` value is 0,
+	// which indicates that "Daylight Saving Time" adjustments for the timezone are disabled.
+	// Don't return the timezone name, instead return Etc/GMT+offset.
+	if dstOff {
+		location, err := time.LoadLocation(timezone)
+		if err != nil {
+			return "", fmt.Errorf("time.LoadLocation() returned error for IANA timeZone: %v, error: %v", timezone, err.Error())
+		}
+		hasDst, stdOffset, _ := getDstInfo(location)
+
+		// The DST is turned off in the Windows configuration,
+		// but this timezone doesn't have DST, so it doesn't matter.
+		if !hasDst {
+			return timezone, nil
+		}
+
+		if stdOffset == nil {
+			return "", fmt.Errorf("%s claims to not have a non-DST time", winTZname)
+		}
+
+		// Can't convert this to an hourly offset.
+		if *stdOffset%3600 != 0 {
+			return "", fmt.Errorf("cannot support disabling DST in the %s zone", winTZname)
+		}
+
+		// This has whole hours as offset.
+		// Return GMT offset as Etc/GMT+offset
+		return fmt.Sprintf("Etc/GMT%+.0f", float64(-*stdOffset)/3600), nil
 	}
-	return strings.TrimSpace(string(data)), nil
+
+	return timezone, nil
 }
 
 // localTZfromReg obtains the time zone Windows is configured to use from registry.
-func localTZfromReg() (string, error) {
+func localTZfromReg() (string, bool, error) {
 	k, err := registry.OpenKey(registry.LOCAL_MACHINE, tzKey, registry.QUERY_VALUE)
 	if err != nil {
-		return "", err
+		return "", false, err
 	}
 	defer k.Close()
 
 	winTZname, _, err := k.GetStringValue(tzKeyVal)
 	if err != nil {
-		return "", err
+		return "", false, err
 	}
-	return strings.TrimSpace(winTZname), nil
+
+	// Get the `DynamicDaylightTimeDisabled` key value, which indicates if DST is enabled or disabled.
+	dstOff, _, err := k.GetIntegerValue(tzDynamicDayLightDisabledKeyVal)
+	if err != nil {
+		dstOff = 0 // Assume DST is not disabled if the value cannot be read
+	}
+
+	return strings.TrimSpace(winTZname), dstOff == 1, nil
+}
+
+// getDstInfo determines if the timezone observes DST and retrieves the standard and DST offsets.
+func getDstInfo(location *time.Location) (bool, *int, *int) {
+	var hasDst bool
+	var stdOffset, dstOffset *int
+
+	now := time.Now()
+	year := now.Year()
+	for _, dt := range []time.Time{time.Date(year, 1, 1, 0, 0, 0, 0, location), time.Date(year, 6, 1, 0, 0, 0, 0, location)} {
+		_, offset := dt.Zone()
+		if dt.IsDST() {
+			dstOffset = &offset
+			hasDst = true
+		} else {
+			stdOffset = &offset
+		}
+	}
+
+	return hasDst, stdOffset, dstOffset
 }
